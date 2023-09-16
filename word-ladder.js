f155e2402079fbf36fcd9ea5d2ebbfc6b42b664c
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-09-16 22:31:49 +0530

    Create word-ladder.js

---
+++ word-ladder.js
@@ -0,0 +1,32 @@
+ const wordSet = new Set(wordList);
+    if (!wordSet.has(endWord)) return 0;
+
+    const queue = [];
+    queue.push(beginWord);
+
+    let level = 1;
+
+    while (queue.length > 0) {
+        const size = queue.length;
+
+        for (let i = 0; i < size; i++) {
+            const currentWord = queue.shift();
+
+            if (currentWord === endWord) return level;
+
+            for (let j = 0; j < currentWord.length; j++) {
+                for (let k = 97; k <= 122; k++) {
+                    const newWord = currentWord.slice(0, j) + String.fromCharCode(k) + currentWord.slice(j + 1);
+
+                    if (wordSet.has(newWord)) {
+                        queue.push(newWord);
+                        wordSet.delete(newWord);
+                    }
+                }
+            }
+        }
+
+        level++;
+    }
+
+    return 0;
