0b312ae42977670aea6e108bd19e6f173666934d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-11-10 22:12:50 +0530

    Update orderly-queue.js

--- orderly-queue.js
+++ orderly-queue.js
@@ -1,1 +1,15 @@
+function getLexicographicallySmallestString(s, k) {
+    let result = '';
+
+    while (k > 0) {
+        let smallestChar = s[0];
+        let smallestIndex = 0;
+
+        // Find the smallest character within the first k characters
+        for (let i = 1; i <= k; i++) {
+            if (s[i] < smallestChar) {
+…    }
+
+    return result;
+}
 
