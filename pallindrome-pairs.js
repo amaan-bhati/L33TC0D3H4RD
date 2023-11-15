3f68658010938a1091185b5ab19f76cdb17728a8
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-11-15 23:44:40 +0530

    Update pallindrome-pairs.js

--- pallindrome-pairs.js
+++ pallindrome-pairs.js
@@ -1,1 +1,15 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.index = -1;
+        this.palindromeIndices = [];
+    }
+}
+
+function isPalindrome(str, start, end) {
+    while (start < end) {
+…    }
+
+    return result;
+}
 
