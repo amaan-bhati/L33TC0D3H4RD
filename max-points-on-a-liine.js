cb3d1efad37aee1b0ceff77dbd02c3562651a7e3
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-02 22:49:56 +0530

    Update max-points-on-a-liine.js

--- max-points-on-a-liine.js
+++ max-points-on-a-liine.js
@@ -1,1 +1,16 @@
 
+var maxPoints = function (points) {
+    if (points.length === 1) return 1 
+    const slopes = {} 
+    let maxSet = 0
+
+    for (let i = 0; i < points.length - 1; i++) {
+        for (let j = i + 1; j < points.length; j++) {
+            const [x1, y1] = points[i]
+            const [x2, y2] = points[j]
+
+…        }
+    }
+
+    return maxSet
+};
