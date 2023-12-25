002e75a0d1cf6d20726cac0ffcf44d5eee51a48e
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-25 23:49:39 +0530

    Create count-of-range-sum.js

---
+++ count-of-range-sum.js
@@ -0,0 +1,14 @@
+function Bisect() {
+    return { insort_right, insort_left, bisect_left, bisect_right }
+    function insort_right(a, x, lo = 0, hi = null) {
+        lo = bisect_right(a, x, lo, hi);
+        a.splice(lo, 0, x);
+    }
+    function bisect_right(a, x, lo = 0, hi = null) { // > upper_bound
+        if (lo < 0) throw new Error('lo must be non-negative');
+        if (hi == null) hi = a.length;
+        while (lo < hi) {
+…        st.update(idx + 1, ++f[idx + 1]);
+    }
+    return res;
+};
