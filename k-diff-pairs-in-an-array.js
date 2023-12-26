8ee6599e7596819057dc8c95f21dcda1da54a04a
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-27 00:00:14 +0530

    Update k-diff-pairs-in-an-array.js

--- k-diff-pairs-in-an-array.js
+++ k-diff-pairs-in-an-array.js
@@ -1,1 +1,18 @@
 
+function findPairs(nums: number[], k: number): number {
+    let left = 0, right = 1, pairs = 0;
+    nums = nums.sort((a, b) => a - b);
+
+    while (left < right && right < nums.length) {
+        if ((nums[right] - nums[left]) < k) {
+            right++;
+        } else if ((nums[right] - nums[left]) > k) {
+            if (right === left + 1) {
+                right++;
+…                right = left + 1;
+            }
+        }
+    }
+
+    return pairs;
+};
