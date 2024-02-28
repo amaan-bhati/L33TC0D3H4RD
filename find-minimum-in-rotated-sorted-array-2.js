95c7658bbee1895461902bc9e91edc54de977004
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-02-28 23:22:07 +0530

    Update find-minimum-in-rotated-sorted-array-2.js

--- find-minimum-in-rotated-sorted-array-2.js
+++ find-minimum-in-rotated-sorted-array-2.js
@@ -1,1 +1,20 @@
 
+var findMin = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else if (nums[mid] < nums[right]) {
+            right = mid;
+        } else {
+            // Handle the case where nums[mid] and nums[right] are equal
+            right--;
+        }
+    }
+
+    return nums[left];
+};
