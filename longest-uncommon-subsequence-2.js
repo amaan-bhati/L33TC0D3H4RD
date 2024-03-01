6ef55a250952da70a88e1cf1d5e8995c4dc8a260
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-03-01 20:53:28 +0530

    Update longest-uncommon-subsequence-2.js

--- longest-uncommon-subsequence-2.js
+++ longest-uncommon-subsequence-2.js
@@ -1,1 +1,31 @@
 
+
+var findLUSlength = function (strs) {
+    let maxLength = -1;
+    for (let i = 0; i < strs.length; i++) {
+        let isSubsequence = false;
+        for (let j = 0; j < strs.length; j++) {
+            if (i === j) {
+                continue;
+            }
+            if (isSubseq(strs[i], strs[j])) {
+                isSubsequence = true;
+                break;
+            }
+        }
+        if (!isSubsequence) {
+            maxLength = Math.max(maxLength, strs[i].length);
+        }
+    }
+    return maxLength;
+};
+
+function isSubseq(s1, s2) {
+    let i = 0;
+    for (let j = 0; j < s2.length && i < s1.length; j++) {
+        if (s1.charAt(i) === s2.charAt(j)) {
+            i++;
+        }
+    }
+    return i === s1.length;
+}
